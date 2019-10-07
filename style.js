1ccae0a7f9ee341db18d0df3a10d62bd8a7f1651
moiseiev-alexey <alexeym.zp@gmail.com>
2019-10-07 22:15:11 +0300

    home-2

--- style.js
+++ style.js
@@ -26,7 +26,124 @@ calc(parseInt(oneNum), parseInt(twoNum),operation);
 var quest = confirm('Хотите повторить?');
 if (quest == true){
     window.location.reload();
+};
+
+//Calculator end//
+///////----------------------///////
+///////----------------------//////
+
+var family = {
+    name: 'Alex',
+    surname: 'Moiseev',
+    age: 30,
+    gender: 'man',
+    obj: {
+        name: 'Evgeniy',
+        surname: 'Moiseiev',
+        age: 34,
+        gender: 'man',
+    },
+    brothers: [
+        {
+            name: 'Evgeniy',
+            surname: 'Moiseiev',
+            age: 34,
+            gender: 'man',
+        },
+        {
+            name: 'Anton',
+            surname: 'Popov',
+            age: 33,
+            gender: 'man',
+        },
+        {
+            name: 'Alexandr',
+            surname: 'Volov',
+            age: 41,
+            gender: 'man',
+            sisters: [
+                {
+                    name: 'Alexandra',
+                    surname: 'Voloshina',
+                    age: 25,
+                    weight: 52,
+                },
+                {
+                    name: 'Natalya',
+                    surname: 'Osipova',
+                    age: 27,
+                    weight: 53,
+                    brothers: [
+                        {
+                            name: 'Viktor',
+                            surname: 'Malov',
+                            age: 27,
+                            gender: 'man'
+                        },
+                        {
+                            name: 'Oleg',
+                            surname: 'Tronov',
+                            age: 35,
+                            gender: 'man',
+                            sisters: [
+                                {
+                                    name: 'Katya',
+                                    surname: 'Orlova',
+                                    age: 22,
+                                    weight: 50,
+                                },
+                                {
+                                    name: 'Vera',
+                                    surname: 'Volya',
+                                    age: 21,
+                                    weight: 45,
+                                    sisters: [
+                                        {
+                                            name: 'Kira',
+                                            surname: 'Ogurtcova',
+                                            age: 30,
+                                            weight: 60,
+                                            brothers: [
+                                                {
+                                                    name: 'Senya',
+                                                    surname: 'Mimikov',
+                                                    age: 15,
+                                                    gender: 'man',
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+};
+
+var totalAge = 0;
+var totalWeight = 0;
+
+function calc (obj) {
+    var array = Object.keys (obj);
+    array.forEach ((item, index) => {
+        var valueInKey = obj[item];
+        var isType =typeof valueInKey
+        if (isType == 'number' && item == 'age'){
+            totalAge +=  valueInKey
+        } else if (isType == 'number' && item == "weight"){
+            totalWeight +=  valueInKey
+        } else if (Array.isArray(valueInKey)){
+            valueInKey.forEach ((name, nameIndex) => calc (name))
+        }
+    })
 }
+
+calc (family);
+console.log (totalAge, totalWeight, totalAge +  totalWeight)
+
 
 
 
